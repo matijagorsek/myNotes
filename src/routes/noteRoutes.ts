f11dc14b09a4a478e920aafa78d9dbef280cc837
matija <matija.gorshek@gmail.com>
2024-02-19 13:42:57 +0100

    update naming in validators

--- src/routes/noteRoutes.ts
+++ src/routes/noteRoutes.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import { createNote, updateNote, deleteNote, getAllNotes, getAllNotesByAuthor, getNoteById } from '../controllers/NoteController';
-import { validateId, validateAuthorId } from '../validators/validators';
+import { validateId } from '../validators/validators';
 
 
 const router = express.Router();
 
 router.post('/', createNote);
 router.get('/', getAllNotes);
-router.get('/byAuthor', validateAuthorId, getAllNotesByAuthor);
+router.get('/byAuthor', validateId, getAllNotesByAuthor);
 router.get('/:id', validateId, getNoteById)
 router.patch('/:id', validateId, updateNote);
 router.delete('/:id', validateId, deleteNote);
--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -4,18 +4,8 @@ import { validate } from "uuid";
 
 export const validateId = (req: Request, res: Response, next: NextFunction) => {
     const noteId = req.params.id;
-    console.log("ID",noteId)
     if (!validate(noteId)) {
       return res.status(400).json({ message: 'Invalid note ID.' });
-    }
-    next();
-  };
-
-  export const validateAuthorId = (req: Request, res: Response, next: NextFunction) => {
-    const noteId = req.params.authorId;
-  
-    if (!validate(noteId)) {
-      return res.status(400).json({ message: 'Invalid author ID.' });
     }
     next();
   };
