1c36487862bf707b6f6722d676116d15074b7327
matija <matija.gorshek@gmail.com>
2024-02-19 09:52:56 +0100

    move utilties to the seperate package

--- src/controllers/noteControllers.ts
+++ src/controllers/noteControllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import Note from '../models/noteModel';
-import { generateUniqueId, getTimestamp } from '../utilities';
+import { generateUniqueId, getTimestamp } from '../utilities/utilities';
 
 let notes: Note[] = [];
 
